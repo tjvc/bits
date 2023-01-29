43be285a041607530ddda78775e98b9817f7d15f
Thom Carter <thom.carter@immersivelabs.com>
2023-01-29 13:01:48 +0000

    Add and configure Prettier
    https://prettier.io/docs/en/integrating-with-linters.html
    https://github.com/prettier/eslint-config-prettier

--- src/parse.ts
+++ src/parse.ts
@@ -1,19 +1,19 @@
-import fs from 'fs/promises';
+import fs from "fs/promises";
 
 type List = (string | number | List)[];
 
 async function parse(path: string): Promise<string | number | List> {
-  const data = await fs.readFile(path)
+  const data = await fs.readFile(path);
   const str = data.toString().trim();
 
   return decode(str);
 }
 
 function decode(str: string): [string | number | List, string] {
-  if (str[0] == 'i') {
+  if (str[0] == "i") {
     const [value, remainder] = decodeInt(str);
     return [value, remainder];
-  } else if (str[0] == 'l') {
+  } else if (str[0] == "l") {
     const [value, remainder] = decodeList(str);
     return [value, remainder];
   } else {
@@ -23,17 +23,17 @@ function decode(str: string): [string | number | List, string] {
 }
 
 function decodeInt(str: string): [number, string] {
-  const parts = str.split('e')
+  const parts = str.split("e");
   const value = parseInt(parts[0].slice(1));
 
-  return [value, parts.slice(1).join('e')]
+  return [value, parts.slice(1).join("e")];
 }
 
 function decodeList(str: string): [List, string] {
   const list = [];
   str = str.slice(1);
 
-  while (str[0] != 'e') {
+  while (str[0] != "e") {
     const [value, remainder] = decode(str);
     list.push(value);
     str = remainder;
@@ -43,7 +43,7 @@ function decodeList(str: string): [List, string] {
 }
 
 function decodeStr(str: string): [string, string] {
-  const parts = str.split(':');
+  const parts = str.split(":");
   const len = parts[0];
   const value = parts[1].slice(0, parseInt(len));
 
