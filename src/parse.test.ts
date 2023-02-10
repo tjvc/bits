19c615a9c37da06a1d53f8b2abcb82612127061d
Thom Carter <thom@tjvc.uk>
2023-02-10 20:29:34 +0000

    Run Prettier

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -1,19 +1,19 @@
-import { describe, expect, test } from '@jest/globals';
-import parse from './parse';
+import { describe, expect, test } from "@jest/globals";
+import parse from "./parse";
 
-describe('parse', () => {
-  test('it decodes a string', async() => {
-    const data = await parse('src/string.torrent');
-    expect(data).toEqual(['test', '']);
+describe("parse", () => {
+  test("it decodes a string", async () => {
+    const data = await parse("src/string.torrent");
+    expect(data).toEqual(["test", ""]);
   });
 
-  test('it decodes an integer', async() => {
-    const data = await parse('src/integer.torrent');
-    expect(data).toEqual([7, '']);
+  test("it decodes an integer", async () => {
+    const data = await parse("src/integer.torrent");
+    expect(data).toEqual([7, ""]);
   });
 
-  test('it decodes a list', async() => {
-    const data = await parse('src/list.torrent');
-    expect(data).toEqual([['test', 7], '']);
+  test("it decodes a list", async () => {
+    const data = await parse("src/list.torrent");
+    expect(data).toEqual([["test", 7], ""]);
   });
 });
