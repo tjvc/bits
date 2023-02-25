6287f7589272edb1071c784df5db9c04236c1ec1
Thom Carter <thom@tjvc.uk>
2023-02-25 18:40:21 +0000

    Make tracker get request
    Spike out the tracker get request. We'll need to refactor this, of
    course.
    
    URLSearchParams are automatically percent-encoded. We don't want this
    for our info hash, which is already encoded, so append that to the URL
    manually.
    
    https.get passes a readable stream to its callback, which emits a
    'data' event when data is available to be read. More generally, Node
    coordinates asynchronous operations via an event-driven architecture.
    All objects that emit events are instances of EventEmitter, and expose
    an on() function that allows us to attach listener functions to named
    events.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
-import parse, { encodeInfo } from "./parse";
+import parse, { encodeInfo, urlEncode } from "./parse";
 import crypto from "crypto";
+import https from "https";
+import { URL } from "url";
 
 const result = parse(process.argv[2]);
 
 result.then((data) => {
-  console.log(data);
-
   if (data.info) {
     const info = data.info;
     const shasum = crypto.createHash("sha1");
     shasum.update(encodeInfo(info));
     const digest = shasum.digest();
-    console.log(digest);
+    const encoded = urlEncode(digest);
+
+    if (data.announce) {
+      const url = new URL(data.announce.toString());
+
+      const params = {
+        peer_id: "11111111111111111111",
+        port: "6881",
+        uploaded: "0",
+        downloaded: "0",
+        left: "0",
+      };
+
+      const queryParams = new URLSearchParams(params);
+      url.search = queryParams.toString();
+      // info_hash is already percent-encoded, so add it manually
+      url.search += "&info_hash=" + encoded;
+
+      https.get(url, (res) => {
+        res.on("data", (d) => {
+          console.log(d.toString());
+        });
+      });
+    }
   }
 });
