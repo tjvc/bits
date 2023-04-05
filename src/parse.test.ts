28264ddbfcaca77142e6515b46d4e360f8db14e5
Thom Carter <thom@tjvc.uk>
2023-04-05 17:01:43 +0100

    Extract BDecoder
    This doesn't really make sense as a class: it doesn't have any state.
    
    I want to retain the ability to test decoding of specific types if
    possible, because I've found those unit tests to be valuable.

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -1,45 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
-import {
-  decodeString,
-  decodeInt,
-  decodeList,
-  decodeDict,
-  encodeInfo,
-  urlEncode,
-} from "./parse";
+import { encodeInfo, urlEncode } from "./parse";
 
 describe("parse", () => {
-  test("it decodes a string buffer", async () => {
-    const output = decodeString(Buffer.from("4:test"));
-    expect(output).toEqual([Buffer.from("test"), Buffer.from("")]);
-  });
-
-  test("it decodes an integer buffer", async () => {
-    const output = decodeInt(Buffer.from("i7e"));
-    expect(output).toEqual([7, Buffer.from("")]);
-  });
-
-  test("it decodes a list buffer", async () => {
-    const output = decodeList(Buffer.from("l4:testi7ee"));
-    expect(output).toEqual([[Buffer.from("test"), 7], Buffer.from("")]);
-  });
-
-  test("it decodes a dict buffer", async () => {
-    const output = decodeDict(Buffer.from("d4:spaml1:a1:bee"));
-    expect(output).toEqual([
-      { spam: [Buffer.from("a"), Buffer.from("b")] },
-      Buffer.from(""),
-    ]);
-  });
-
-  test("it decodes a torrent file buffer", async () => {
-    const output = decodeDict(Buffer.from("d4:spaml1:a1:bee"));
-    expect(output).toEqual([
-      { spam: [Buffer.from("a"), Buffer.from("b")] },
-      Buffer.from(""),
-    ]);
-  });
-
   test("it encodes an info object", async () => {
     const info = {
       length: 2,
--- src/parse.ts
+++ src/parse.ts
@@ -1,84 +1,13 @@
 import fs from "fs/promises";
+import { BDecoder, BDict } from "./b_decoder";
 
-type List = (Buffer | number | List | Dict)[];
-type Dict = {
-  [key: string]: Buffer | number | List | Dict | undefined;
-  info?: Dict;
-};
-
-async function parse(path: string): Promise<Dict> {
+async function parse(path: string): Promise<BDict> {
   const buf = await fs.readFile(path);
-  return decodeDict(buf)[0];
+  const decoder = new BDecoder();
+  return decoder.decodeDict(buf)[0];
 }
 
-export function decode(buf: Buffer): [Buffer | number | List | Dict, Buffer] {
-  if (buf[0] == "i".charCodeAt(0)) {
-    const [value, remainder] = decodeInt(buf);
-    return [value, remainder];
-  } else if (buf[0] == "l".charCodeAt(0)) {
-    const [value, remainder] = decodeList(buf);
-    return [value, remainder];
-  } else if (buf[0] == "d".charCodeAt(0)) {
-    const [value, remainder] = decodeDict(buf);
-    return [value, remainder];
-  } else {
-    const [value, remainder] = decodeString(buf);
-    return [value, remainder];
-  }
-}
-
-export function decodeInt(buf: Buffer): [number, Buffer] {
-  let i = 0;
-
-  while (buf[i] != "e".charCodeAt(0)) {
-    i += 1;
-  }
-  i += 1;
-
-  return [parseInt(buf.slice(1, i + 1).toString()), buf.slice(i)];
-}
-
-export function decodeList(buf: Buffer): [List, Buffer] {
-  const list = [];
-  buf = buf.slice(1);
-
-  while (buf[0] != "e".charCodeAt(0)) {
-    const [value, remainder] = decode(buf);
-    list.push(value);
-    buf = remainder;
-  }
-
-  return [list, buf.slice(1)];
-}
-
-export function decodeDict(buf: Buffer): [Dict, Buffer] {
-  const dict: Dict = {};
-  buf = buf.slice(1);
-
-  while (buf[0] != "e".charCodeAt(0)) {
-    const [key, keyRemainder] = decodeString(buf);
-    const [value, valueRemainder] = decode(keyRemainder);
-    dict[key.toString()] = value;
-    buf = valueRemainder;
-  }
-
-  return [dict, buf.slice(1)];
-}
-
-export function decodeString(buf: Buffer): [Buffer, Buffer] {
-  let i = 0;
-
-  while (buf[i] != ":".charCodeAt(0)) {
-    i += 1;
-  }
-  i += 1;
-
-  const len = parseInt(buf.slice(0, i).toString());
-
-  return [buf.slice(i, i + len), buf.slice(i + len)];
-}
-
-export function encodeInfo(info: Dict): Buffer {
+export function encodeInfo(info: BDict): Buffer {
   let buf = Buffer.from("d");
 
   for (const [key, value] of Object.entries(info)) {
