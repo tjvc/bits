8b5446d708c00d19552d14e705a32764c3c0ad8b
Thom Carter <thom@tjvc.uk>
2023-04-10 16:15:34 +0100

    Extract TrackerRequest
    TODO:
    
    * Stricter typing for params
    * A test - interesting to try mocking a HTTP request
    * Error handling!
    * Extract port to configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { BData } from "./b_data";
 import { Torrent } from "./torrent";
 import { Download } from "./download";
+import { TrackerRequest } from "./tracker_request";
 
 import fs from "fs/promises";
-import https from "https";
 import net from "net";
-import { URL } from "url";
 
 async function main() {
   const buf = await fs.readFile(process.argv[2]);
@@ -14,79 +13,66 @@ async function main() {
   const torrent = new Torrent(data);
 
   const info = torrent.info();
-  const encoded = info.hash().urlEncode();
 
-  const url = new URL(torrent.trackerUrl());
+  const peerId = "11111111111111111111";
+  const trackerRequest = new TrackerRequest(
+    peerId,
+    torrent.trackerUrl(),
+    info.hash()
+  );
+  const trackerResponse = await trackerRequest.send();
 
-  const params = {
-    peer_id: "11111111111111111111",
-    port: "6881",
-    uploaded: "0",
-    downloaded: "0",
-    left: "0",
-  };
-
-  const queryParams = new URLSearchParams(params);
-  url.search = queryParams.toString();
-  // info_hash is already percent-encoded, so add it manually
-  url.search += "&info_hash=" + encoded;
-
-  https.get(url, (res) => {
-    res.on("data", async (d) => {
-      const download = new Download(new BData(d).decode());
-      const peers = download.peers;
-      peers.forEach((peer) => {
-        console.log(peer.ip, peer.port);
-      });
-
-      const handshakeHeader = Buffer.from(
-        "\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00"
-      );
-
-      const peerId = Buffer.from(params.peer_id);
-      const handshake = Buffer.concat([
-        handshakeHeader,
-        info.hash().raw,
-        peerId,
-      ]);
-
-      const peer = peers[Math.floor(Math.random() * peers.length)];
-      const client = net.createConnection(peer.port, peer.ip.toString());
-
-      client.on("connect", () => {
-        console.log("connected");
-      });
-
-      client.on("data", (data) => {
-        console.log(data);
-      });
-
-      client.on("close", () => {
-        console.log("closed");
-      });
-
-      console.log("Sending handshake");
-      client.write(handshake);
-
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-
-      const interested = Buffer.from([0, 0, 0, 1, 2]);
-      console.log("Sending interested");
-      client.write(interested);
-
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-
-      const request = Buffer.alloc(17);
-      request.writeUInt32BE(13);
-      request.writeUInt8(6, 4);
-      request.writeUInt32BE(32, 5);
-      request.writeUInt32BE(0, 9);
-      request.writeUInt32BE(256, 13);
-
-      console.log("Requesting piece");
-      client.write(request);
-    });
+  const download = new Download(new BData(trackerResponse).decode());
+  const peers = download.peers;
+  peers.forEach((peer) => {
+    console.log(peer.ip, peer.port);
   });
+
+  const handshakeHeader = Buffer.from(
+    "\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00"
+  );
+
+  const handshake = Buffer.concat([
+    handshakeHeader,
+    info.hash().raw,
+    Buffer.from(peerId),
+  ]);
+
+  const peer = peers[Math.floor(Math.random() * peers.length)];
+  const client = net.createConnection(peer.port, peer.ip.toString());
+
+  client.on("connect", () => {
+    console.log("connected");
+  });
+
+  client.on("data", (data) => {
+    console.log(data);
+  });
+
+  client.on("close", () => {
+    console.log("closed");
+  });
+
+  console.log("Sending handshake");
+  client.write(handshake);
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const interested = Buffer.from([0, 0, 0, 1, 2]);
+  console.log("Sending interested");
+  client.write(interested);
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const request = Buffer.alloc(17);
+  request.writeUInt32BE(13);
+  request.writeUInt8(6, 4);
+  request.writeUInt32BE(32, 5);
+  request.writeUInt32BE(0, 9);
+  request.writeUInt32BE(256, 13);
+
+  console.log("Requesting piece");
+  client.write(request);
 }
 
 main();
---
+++ src/tracker_request.ts
@@ -0,0 +1,40 @@
+import { InfoHash } from "./info_hash";
+import https from "https";
+
+export class TrackerRequest {
+  private peerId: string;
+  private trackerUrl: string;
+  private infoHash: InfoHash;
+  private params: Record<string, string>;
+
+  constructor(peerId: string, trackerUrl: string, infoHash: InfoHash) {
+    this.peerId = peerId;
+    this.trackerUrl = trackerUrl;
+    this.infoHash = infoHash;
+
+    this.params = {
+      peer_id: this.peerId,
+      port: "6881",
+      uploaded: "0",
+      downloaded: "0",
+      left: "0",
+    };
+  }
+
+  async send(): Promise<Buffer> {
+    const url = new URL(this.trackerUrl);
+    const queryParams = new URLSearchParams(this.params);
+
+    url.search = queryParams.toString();
+    // info_hash is already percent-encoded, so add it manually
+    url.search += "&info_hash=" + this.infoHash.urlEncode();
+
+    return new Promise((resolve) => {
+      https.get(url, (res) => {
+        res.on("data", (data) => {
+          resolve(data);
+        });
+      });
+    });
+  }
+}
